0897de8f95783b7db32f88b443f8435403c16c1c
adnan-khan <akhan823974@gmail.com>
2024-01-07 23:35:00 +0530

    03_basics

---
+++ 03_basics/02_Scopes.js
@@ -0,0 +1,26 @@
+var c = 300
+let a =  300
+if(true){
+    let a = 10
+    const b = 20
+    // console.log("INNRT:" , a);
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+function one(){
+    const  username = "adnan"
+
+    function two(){
+        const  website = "github"
+        console.log(username);
+        console.log(website);
+    }
+    two()
+}
+one()
+
---
+++ 03_basics/04_iffe.js
@@ -0,0 +1,20 @@
+// Immediately Invoked Function Expressions (IIFE)
+// Ans ) to call function immediately andTo avoid pollution on global scope and
+
+// 1)
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+// To avoid error put samicolan 
+
+// ( (name) =>{
+//     console.log((`connected To ${name}`));
+// }) ("adnan");
+
+// 2)
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
+
