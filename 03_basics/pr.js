e0451a695bec2d58f4f9b974479b586ab9b051e5
adnan-khan <akhan823974@gmail.com>
2024-01-08 19:23:23 +0530

    completed iterations/functions

--- 03_basics/pr.js
+++ 03_basics/pr.js
@@ -1,56 +1,10 @@
-//var c = 300
-let a = 300
-if (true) {
-    let a = 10
-    const b = 20
-    // console.log("INNER: ", a);
-    
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
 }
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
-
-
-function one(){
-    const username = "hitesh"
-
-    function two(){
-        const website = "youtube"
-        console.log(username);
-    }
-    // console.log(website);
-
-     two()
-
-}
-
-// one()
-
-if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
-        const website = " youtube"
-        // console.log(username + website);
-    }
-    // console.log(website);
-}
-
-// console.log(username);
-
-
-// ++++++++++++++++++ interesting ++++++++++++++++++
-
-
-console.log(addone(5))
-
-function addone(num){
-    return num + 1
-}
-
-
-
-addTwo(5)
-const addTwo = function(num){
-    return num + 2
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
 }
