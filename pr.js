5da08505f836305e2b5166b600d431535d289f8a
adnan-khan <akhan823974@gmail.com>
2024-01-16 22:50:49 +0530

    classes

--- pr.js
+++ pr.js
@@ -1,2 +1,22 @@
+const product = {
+        name: "parkar pen (black)",
+        star: 5,
+        dealOfTheDay: true,
+        price: 39
 
-        const promiseOne = new Pro
+}
+
+console.table([product]);
+
+const profile = {
+        post : 195,
+        followers :"569k" ,
+        following: 4,
+        name : "Sharradha khapra",
+        link : "@shardhakapra",
+        Work :" Enterperner",
+        bio : "Apna college | Ex-microsoft , DRDO"
+
+}
+
+console.table(profile);
