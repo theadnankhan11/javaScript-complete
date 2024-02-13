1a8c0004fcc94858b2b2caeff3ea1f60f5251d41
adnan-khan <akhan823974@gmail.com>
2024-02-13 22:37:13 +0530

    save changes in all files

--- 05_iterations/6_six.js
+++ 05_iterations/6_six.js
@@ -1,10 +1,10 @@
-// const coding = ["js", "ruby", "java", "python", "cpp"]
-// const values = coding.forEach( (item) => {
-//     //console.log(item);
-//     return item
-// } )
+const coding = ["js", "ruby", "java", "python", "cpp"]
+const values = coding.forEach( (item) => {
+    console.log(item);
+    return item
+} )
 
-// console.log(values);
+console.log(values);
 
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
